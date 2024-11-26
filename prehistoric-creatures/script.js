a9497a621d51fe46d8fd5547be8ea0be550f5983
Rosa Moriya <155775675+rosamoriya@users.noreply.github.com>
2024-11-26 15:45:45 -0500

    Delete prehistoric-creatures directory

--- prehistoric-creatures/script.js
+++
@@ -1,9 +0,0 @@
-// Placeholder for future interactive functionality
-console.log("Script loaded!");
-
-// Example: Adding functionality to "Learn More" button
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", () => {
-    alert("Learn more about this dinosaur!");
-  });
-});
